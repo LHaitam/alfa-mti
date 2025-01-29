f8a571e97539d56071930ec9e1bd83fadeaae66e
LHaitam <lourhmihaitam@outlook.fr>
2025-01-29 08:09:41 +0100

    first commit

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import FlyoutNav from "./nav/Header"; // Si exporté par défaut
+import FlyoutNav from "./nav/Header";
 import Hero from "./hero/Hero";
 import { About } from "./about/About";
 import { Sectors } from "./sectors/sectors";
@@ -10,32 +10,22 @@ import Footer from "./footer/Footer";
 
 export const HomPage = () => {
   return (
-    <div className="grid bg-white min-h-screen">
+    <div className="grid bg-white min-h-screen overflow-x-hidden">
       <FlyoutNav />
-      <main>
+      <main className="max-w-screen overflow-hidden">
         <div className="space-y-32 pb-24">
           {/* Hero section with full width */}
-          <div className="w-full min-h-screen z-10 bg-black bg-opacity-10">
+          <div className="w-full max-w-full min-h-screen z-10 bg-black bg-opacity-10">
             <Hero
-              title={
-                "ALFA MAROC TECH INDUSTRIE"
-              }
-              subtitle={
-                "Souverainetés Industrielle Marocaine"
-              }
+              title={"ALFA MAROC TECH INDUSTRIE"}
+              subtitle={"Souveraineté Industrielle Marocaine"}
               buttonText="Découvrez notre catalogue"
               buttonLink="/catalogue.pdf"
               bgVideo="/bg.mp4"
-            />          </div>
+            />
+          </div>
           {/* Rest of the content */}
-          <div className="mx-auto max-w-5xl px-4 md:px-8 space-y-32">
-            {/* <div className="w-full">
-              <img
-                src="/images/amti.jpg"
-                alt="Présentation d'AMTI"
-                className="w-full rounded-2xl shadow-lg"
-              />
-            </div> */}
+          <div className="container mx-auto max-w-5xl px-4 md:px-8 space-y-32 overflow-hidden">
             <About />
             <Strength />
             <Sectors />
